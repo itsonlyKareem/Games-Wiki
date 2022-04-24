1b6e46e83ca1cdc5833b7d6d64c8c02f2a26c349
itsonlyKareem <itsonlykareem@gmail.com>
2022-04-24 11:50:20 +0200

    Work Started

---
+++ app/src/main/java/views/MainActivity.kt
@@ -0,0 +1,12 @@
+package views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.omega.gamestar.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/views/SplashActivity.kt
@@ -0,0 +1,28 @@
+package views
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import com.omega.gamestar.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+        setContentView(R.layout.activity_splash)
+
+        val instance = this
+
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(2000L)
+            val intent = Intent(instance, IntroActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
